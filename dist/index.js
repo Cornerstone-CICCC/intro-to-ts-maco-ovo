31effc1d241ffc21f834f52114a68f2d8f7f9580
makoto2025 <aratamakoto2025@gmail.com>
2026-04-14 01:45:34 -0700

    num1 done

---
+++ dist/index.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+function doubleNumber(num) {
+    return num * 2;
+}
+console.log(doubleNumber(20));
+//# sourceMappingURL=index.js.map
---
+++ index.ts
@@ -0,0 +1,15 @@
+//  1. Number Manipulation
+
+function doubleNumber(num: number): number {
+	return num * 2;
+}
+
+console.log(doubleNumber(20));
+
+const squareNumber = (num:number): number => {
+return num * num
+}
+
+console.log(squareNumber(11));
+
+//  2. Boolean Logic
